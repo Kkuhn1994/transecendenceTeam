96c67a13d4d0d88db553e91d66a9899feabf1372
Kevin Kuhn <kkuhn@3-C-10.42heilbronn.de>
2025-10-30 15:39:27 +0100

    database sertup finished

--- src/login_service/service/index.js
+++ src/login_service/service/index.js
@@ -30,46 +30,27 @@ const path = require('path')
 
 fastify.post('/', async (request, reply) => {
 
+ const sqlite3 = require('sqlite3');
+ const db = new sqlite3.Database('/app/data/database.db');
 
-  console.log('/ receivged');
-  // const db = new sqlite3.Database('/app/data/database.db');
   fastify.log.info('📦 Request Body:', request.body)
   const { email, password } = request.body;
-  console.log( email + password);
-  
-  console.log(request.body);
-  reply.send({ received: request.body })
-  //   db.run(
-  // `INSERT INTO users (email, password) VALUES (?, ?)`,
-  // [email, password],
-  // function (err) {
-  //   if (err) {
-  //     fastify.log.error('❌ DB Error:', err);
-  //     reply.status(500).send({ error: 'Database error' });
-  //   } else {
-  //     fastify.log.info(`✅ Inserted row with ID ${this.lastID}`);
-  //     reply.send({ id: this.lastID, received: request.body });
-  //   }
-  // }
-  // );
+  db.run(
+  `INSERT INTO users (email, password) VALUES (?, ?)`,
+  [email, password],
+  function (err) {
+    if (err) {
+      fastify.log.error('❌ DB Error:', err);
+    } else {
+      fastify.log.info(`✅ Inserted row with ID ${this.lastID}`);
+    }
+  }
+  );
 })
 
-fastify.get('/messages', (request, reply) => {
-  db.all('SELECT * FROM users', [], (err, rows) => {
-    if (err) {
-      reply.status(500).send({ error: 'Database read error' });
-    } else {
-      reply.send(rows);
-    }
-  });
-});
-
-// Run the server!
 fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
-  //  fastify.log.info(`Server running at ${address}`)
 })
